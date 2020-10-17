269a98f94ebe86c7c9c3b1843d4e76700c8d0640
Haris <hariskhan1435@gmail.com>
2020-10-17 23:02:51 +0500

    First commit

---
+++ build/precache-manifest.bc338e63aaeb1fa7f52f82afc6b38839.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f2e66b6ebafb124f4470585af40931",
+    "url": "/index.html"
+  },
+  {
+    "revision": "566d5d0035fcc9ae77a9",
+    "url": "/static/css/main.93da1160.chunk.css"
+  },
+  {
+    "revision": "c2d246ecc2b83ccac6a6",
+    "url": "/static/js/2.8b77c9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8b77c9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "566d5d0035fcc9ae77a9",
+    "url": "/static/js/main.7a67164b.chunk.js"
+  },
+  {
+    "revision": "8e73d7389c9fd7d72264",
+    "url": "/static/js/runtime-main.691d0382.js"
+  }
+]);
